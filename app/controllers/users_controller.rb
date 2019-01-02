e32c894108fe6f145431ae101af14ab0090ebe62
Jackson_supervised <jackson_supervised@mbpro.lan>
2019-01-02 14:05:55 -0600

    made google form that creates new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,33 @@
-class UsersController < ApplicationController
+	class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
+		def index
+			@users = User.all
+		end
 
-  def show
-    @users = User.all
-    puts "-------------- lal la la the id is :  #{params[:id]} "
+		def show
+			@users = User.all
+			puts "-------------- lal la la the id is :  #{params[:id]} "
 
-    @user = User.find_by(id: params[:id])
-  end
+			@user = User.find_by(id: params[:id])
+		end
 
-end
+
+		def new
+			@user = User.new
+		end
+
+
+
+		def create
+			@user = User.new(user_params)
+			if @user.save
+				redirect_to user_path(@user)
+			end
+		end
+
+private
+
+		def user_params 
+			params.required(:user).permit(:firstName, :lastName)
+		end
+	end
